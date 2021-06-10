92ef2defb51174d6c71cfad0fd2a90e2cd81f226
Cong Wang <cwang@rciadmins-MBP.attlocal.net>
2021-06-09 23:35:21 -0400

    Implementing MQ

---
+++ MessageQueue.py
@@ -0,0 +1,34 @@
+import pika
+
+class MetaClass(type):
+    _instance ={}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instance:
+            cls._instance[cls] = super(MetaClass, cls).__call__(*args, **kwargs)
+            return cls._instance[cls]
+
+class RabbitmqConfigure(metaclass=MetaClass):
+
+    def __init__(self, queue='hello', host='localhost', routingKey='hello', exchange=''):
+        #Configure Rabbit Mq Server
+        self.queue = queue
+        self.host = host
+        self.routingKey = routingKey
+        self.exchange = exchange
+
+
+class RabbitMq():
+
+    def __init__(self, server):
+        self.server = server
+        self._connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.server.host))
+        self._channel = self._connection.channel()
+        self._channel.queue_declare(queue=self.server.queue)
+
+    def publish(self, msg ={}):
+        self._channel.basic_publish(exchange=self.server.exchange,
+                      routing_key=self.server.routingKey,
+                      body=str(msg))
+
+        print("Published Message: {}".format(msg))
+        self._connection.close()
