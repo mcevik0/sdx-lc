dd89b45fabf674452235b1ac1ad365ff5d058281
Cong Wang <cwang@rciadmins-MBP.attlocal.net>
2021-06-30 22:21:40 -0400

    add mq rpc

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -3,18 +3,24 @@
 import connexion
 
 from swagger_server import encoder
-from swagger_server.messaging.message_queue import *
-
+from swagger_server.messaging.message_queue_producer import *
+from swagger_server.messaging.rpc_queue_producer import *
 
 def main():
-    serverconfigure = RabbitmqConfigure(queue='hello',
-                               host='localhost',
-                               routingKey='hello',
-                               exchange='')
+    # serverconfigure = RabbitmqConfigure(queue='hello',
+    #                            host='localhost',
+    #                            routingKey='hello',
+    #                            exchange='')
 
-    rabbitmq = RabbitMq(serverconfigure)
-    # Testing message
-    #rabbitmq.publish(body={"localctlr":1})
+    # rabbitmq = RabbitMq(serverconfigure)
+    # # Testing message
+    # rabbitmq.publish(body={"localctlr":1})
+
+    # message queue rpc test
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='rpc_queue')
+
 
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
