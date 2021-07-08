6151a86d3730f29f5e4efeb5cfbed6d95ae87ba3
Cong Wang <cwang@rciadmins-MBP.attlocal.net>
2021-07-07 22:33:55 -0400

    db implementation

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -6,7 +6,33 @@ from swagger_server import encoder
 from swagger_server.messaging.message_queue_producer import *
 from swagger_server.messaging.rpc_queue_producer import *
 
+from optparse import OptionParser
+import argparse
+from swagger_server.utils.db_utils import *
+
 def main():
+    parser = OptionParser()
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("-m", "--manifest", dest="manifest", type=str, 
+                        action="store", help="specifies the manifest")
+
+    parser.add_argument("-d", "--database", dest="database", type=str, 
+                         action="store", help="Specifies the database ", 
+                         default=":memory:")
+
+    options = parser.parse_args()
+    print(options.manifest)
+
+    dbname = options.database
+    # Get DB connection and tables set up.
+    db_tuples = [('config_table', "test-config")]
+    
+    db_util = DbUtils()
+    db_util._initialize_db(dbname, db_tuples)
+    
+
     # serverconfigure = RabbitmqConfigure(queue='hello',
     #                            host='localhost',
     #                            routingKey='hello',
@@ -21,7 +47,7 @@ def main():
     channel = connection.channel()
     channel.queue_declare(queue='rpc_queue')
 
-
+    # Run swagger service
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'SDX LC'}, pythonic_params=True)
