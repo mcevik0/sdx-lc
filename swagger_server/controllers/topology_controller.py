e52cff18ffee4e7b45b839d5e92c791c4aa92e15
Cong Wang - RENCI <cwang@aw-sdx-lc-1.renci.org>
2021-10-21 11:08:17 -0400

    touch up mq rpc

--- swagger_server/controllers/topology_controller.py
+++ swagger_server/controllers/topology_controller.py
@@ -3,6 +3,7 @@ import six
 import os
 import json
 import logging
+import threading
 
 from swagger_server.models.api_response import ApiResponse  # noqa: E501
 from swagger_server.models.topology import Topology  # noqa: E501
@@ -30,7 +31,8 @@ db_tuples = [('config_table', "test-config")]
 db_instance = DbUtils()
 db_instance._initialize_db(DB_NAME, db_tuples)
 
-rpc = RpcClient()
+# initiate rpc producer with 5 seconds timeout
+rpc = RpcProducer(5)
 
 def add_topology(body):  # noqa: E501
     """Send a new topology to SDX-LC
@@ -52,12 +54,8 @@ def add_topology(body):  # noqa: E501
     db_instance.add_key_value_pair_to_db('test', json_body)
     print('Saving to database complete.')
 
-
-    print("Published Message: {}".format(body))
-    # response = publisher.call(json_body)
-    # message_number = publisher.publish_message()
+    print("Published Message to MQ: {}".format(body))
     response = rpc.call(json_body)
-    print('Published message.')
 
     return str(response)
 
--- swagger_server/messaging/rpc_queue_producer.py
+++ swagger_server/messaging/rpc_queue_producer.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python
 import pika
 import uuid
+import os
+import time
+import threading
 
-class RpcClient(object):
-    def __init__(self):
+MQ_HOST = 'aw-sdx-monitor.renci.org'
+
+class RpcProducer(object):
+    def __init__(self, timeout):
         self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host='aw-sdx-monitor.renci.org'))
+            pika.ConnectionParameters(host=MQ_HOST))
 
         self.channel = self.connection.channel()
+        self.timeout = timeout
 
         result = self.channel.queue_declare(queue='', exclusive=True)
         self.callback_queue = result.method.queue
 
-        self.channel.basic_consume(
-            queue=self.callback_queue,
-            on_message_callback=self.on_response,
-            auto_ack=True)
+        self.channel.basic_consume(queue=self.callback_queue,
+                            on_message_callback=self.on_response,
+                            auto_ack=True)
+
+    def keep_live(self):
+        while True:
+            time.sleep(30)
+            self.connection.process_data_events()
 
     def on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
@@ -24,21 +34,29 @@ class RpcClient(object):
     def call(self, body):
         self.response = None
         self.corr_id = str(uuid.uuid4())
-        self.channel.basic_publish(
-            exchange='',
-            routing_key='rpc_queue',
-            properties=pika.BasicProperties(
-                reply_to=self.callback_queue,
-                correlation_id=self.corr_id,
-            ),
-            body=str(body))
+        self.channel.basic_publish(exchange='',
+                                routing_key='rpc_queue',
+                                properties=pika.BasicProperties(
+                                    reply_to=self.callback_queue,
+                                    correlation_id=self.corr_id,
+                                ),
+                                body=str(body))
+        print("Waiting for response...")
+        timer = 0
         while self.response is None:
+            time.sleep(1)
+            timer += 1
+            # print("Waiting for response..." + str(timer))
+            if timer == self.timeout:
+                return "No response from MQ receiver"
             self.connection.process_data_events()
+
+        self.channel.close()
         return self.response
 
 if __name__ == "__main__":
-    rpc = RpcClient()
-    msg = 'hello!'
-    print(" [x] Sending MQ request")
-    response = rpc.call(msg)
-    print(" [.] Got %r" % response)
+    rpc = RpcProducer()
+    body = "test body"
+    print("Published Message: {}".format(body))
+    response = rpc.call(body)
+    print(" [.] Got response: " + str(response))
