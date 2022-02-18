437fb74ed852368c3755fa9908a419ea046b0412
Cong Wang - RENCI <cwang@aw-sdx-lc-1.renci.org>
2022-02-17 23:32:49 -0500

    adding bidirectional mq

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,56 +1,92 @@
 #!/usr/bin/env python3
 
 import connexion
-import os
-import time
 
 from swagger_server import encoder
-from swagger_server.messaging.message_queue_producer import *
+from swagger_server.messaging.rpc_queue_consumer import *
 from swagger_server.utils.db_utils import *
-# from swagger_server.messaging.rpc_queue_producer import *
 
 from optparse import OptionParser
 import argparse
+import time
+import threading
+import logging
+import json
 
+def is_json(myjson):
+  try:
+    json.loads(myjson)
+  except ValueError as e:
+    return False
+  return True
+
+def start_consumer(thread_queue, db_instance):
+    logger = logging.getLogger(__name__)
+    logging.getLogger("pika").setLevel(logging.WARNING)
+
+    MESSAGE_ID = 0
+    HEARTBEAT_ID = 0
+    
+    rpc = RpcConsumer(thread_queue)
+    t1 = threading.Thread(target=rpc.start_consumer, args=())
+    t1.start()
+
+    while True:
+        if not thread_queue.empty():
+            msg = thread_queue.get()
+            logger.info("MQ received message:" + str(msg))
+            
+            if 'Heart Beat' in str(msg):
+                HEARTBEAT_ID += 1
+                logger.info('Heart beat received. ID: ' + str(HEARTBEAT_ID))
+            else:
+                logger.info('Saving to database.')
+                if is_json(msg):
+                    msg_json = json.loads(msg)
+                    msg_id = msg_json["id"]
+                    name = msg_json["name"]
+                    # lc_msg_version = msg_json["version"]
+                    db_msg_id = str(name) + "-" + str(msg_id)
+                    # print(db_msg_id)
+                    db_instance.add_key_value_pair_to_db(db_msg_id, msg)    
+                else:
+                    db_instance.add_key_value_pair_to_db(MESSAGE_ID, msg) 
+
+                logger.info('Save to database complete.')
+
+                logger.info('message ID:' + str(MESSAGE_ID))
+                value = db_instance.read_from_db(MESSAGE_ID)
+                logger.info('got value back:')
+                logger.info(value)
+                MESSAGE_ID += 1
+                
 
 def main():
     # Sleep 7 seconds waiting for RabbitMQ to be ready
     time.sleep(7)
 
-    MQ_HOST = os.environ.get('MQ_HOST')
-
-    parser = OptionParser()
-
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    parser.add_argument("-m", "--manifest", dest="manifest", type=str, 
-                        action="store", help="specifies the manifest")
-
-    parser.add_argument("-d", "--database", dest="database", type=str, 
-                         action="store", help="Specifies the database ", 
-                         default=":memory:")
-
-    options = parser.parse_args()
-    print(options.manifest)
-
-    # dbname = options.database
-    # Get DB connection and tables set up.
-    # db_tuples = [('config_table', "test-config")]
-    
-    # db_util = DbUtils()
-    # db_util._initialize_db(dbname, db_tuples)
-    
-    # message queue rpc test
-    # connection = pika.BlockingConnection(pika.ConnectionParameters(host=MQ_HOST))
-    # channel = connection.channel()
-    # channel.queue_declare(queue='rpc_queue')
+    logging.basicConfig(level=logging.INFO)
 
     # Run swagger service
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'SDX LC'}, pythonic_params=True)
-    app.run(port=8080)
+    # app.run(port=8080)
+    # Run swagger in a thread
+    threading.Thread(target=lambda: app.run(port=8080)).start()
+    # app.run(port=8080)
 
+    DB_NAME = os.environ.get('DB_NAME') + '.sqlite3'
+    MANIFEST = os.environ.get('MANIFEST')
+
+    # Get DB connection and tables set up.
+    db_tuples = [('config_table', "test-config")]
+
+    db_instance = DbUtils()
+    db_instance._initialize_db(DB_NAME, db_tuples)
+    # amqp_url = 'amqp://guest:guest@aw-sdx-monitor.renci.org:5672/%2F'
+    thread_queue = Queue()
+    start_consumer(thread_queue, db_instance)
 
 if __name__ == '__main__':
     main()
