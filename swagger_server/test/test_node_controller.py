14999ca34a74df1bec073c5fec72dad35e0cc687
Sajith Sasidharan <sajith@hcoop.net>
2022-08-26 10:17:54 -0400

    Refactor node controller test data

--- swagger_server/test/test_node_controller.py
+++ swagger_server/test/test_node_controller.py
@@ -14,38 +14,41 @@ from swagger_server.test import BaseTestCase
 class TestNodeController(BaseTestCase):
     """NodeController integration test stubs"""
 
+    __location = Location(
+        address = "unknown",
+        latitude = 0.0,
+        longitude = 0.0,
+    )
+
+    __ports = [Port(
+        id = "test_node_port_id",
+        name = "test_node_port_name",
+        short_name = "test_node_port_short_name",
+        node = "test_node_id",
+        label_range = None,
+        status = "unknown",
+        state = "unknown",
+        private_attributes = None,
+    )]
+
+    __node = Node(
+        id = "test_node_id",
+        name = "test_node_name",
+        short_name = "test_node_short_name",
+        location = __location,
+        ports = __ports,
+        private_attributes = None,
+    )
+
     def test_add_node(self):
         """Test case for add_node
 
         add a new node to the topology
         """
-        location = Location(
-            address = "unknown",
-            latitude = 0.0,
-            longitude = 0.0,
-        )
-        ports = [Port(
-            id = "test_add_node_port_id",
-            name = "test_add_node_port_name",
-            short_name = "test_add_node_port_short_name",
-            node = "test_add_node_id",
-            label_range = None,
-            status = "unknown",
-            state = "unknown",
-            private_attributes = None,
-        )]
-        body = Node(
-            id = "test_add_node_id",
-            name = "test_add_node_name",
-            short_name = "test_add_node_short_name",
-            location = location,
-            ports = ports,
-            private_attributes = None,
-        )
         response = self.client.open(
             "/SDX-LC/1.0.0/node",
             method="POST",
-            data=json.dumps(body),
+            data=json.dumps(self.__node),
             content_type="application/json",
         )
         self.assert200(response, "Response body is : " + response.data.decode("utf-8"))
@@ -104,7 +107,7 @@ class TestNodeController(BaseTestCase):
         response = self.client.open(
             "/SDX-LC/1.0.0/node",
             method="PUT",
-            data=json.dumps(body),
+            data=json.dumps(self.__node),
             content_type="application/json",
         )
         self.assert200(response, "Response body is : " + response.data.decode("utf-8"))
