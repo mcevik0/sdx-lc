cb40a554dfcec235fdd9dd8567f2ed5542ce20cd
Cong Wang <cwang@renci.org>
2023-03-23 22:02:00 -0400

    add some debug messages

--- swagger_server/jobs/pull_topo_changes.py
+++ swagger_server/jobs/pull_topo_changes.py
@@ -7,12 +7,12 @@ import threading
 import time
 import urllib.request
 
-from messaging.rpc_queue_producer import *
-from utils.db_utils import *
-
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 )
+
+from messaging.rpc_queue_producer import *
+from utils.db_utils import *
 
 
 DOMAIN_CONTROLLER_PULL_URL = os.environ.get("DOMAIN_CONTROLLER_PULL_URL")
@@ -30,11 +30,11 @@ def main():
 def process_domain_controller_topo(db_instance):
     while True:
         latest_topology = db_instance.read_from_db("latest_topology")
-        json_latest_topology = json.loads(latest_topology["latest_topology"])
-
-        if not json_latest_topology:
+        if not latest_topology:
             time.sleep(int(DOMAIN_CONTROLLER_PULL_INTERVAL))
             continue
+
+        json_latest_topology = json.loads(latest_topology["latest_topology"])
 
         try:
             latest_topo_version = json_latest_topology["version"]
@@ -48,6 +48,8 @@ def process_domain_controller_topo(db_instance):
             time.sleep(int(DOMAIN_CONTROLLER_PULL_INTERVAL))
             continue
 
+        logger.debug("Pulled request from domain controller")
+
         json_pulled_topology = json.loads(pulled_topology)
         try:
             pulled_topo_version = json_pulled_topology["version"]
@@ -59,11 +61,12 @@ def process_domain_controller_topo(db_instance):
             time.sleep(5)
             continue
 
-        logger.debug("Adding pulled topo to db")
+        logger.debug("Pulled topo with different version. Adding pulled topo to db")
         db_instance.add_key_value_pair_to_db(
             "topoVersion" + str(json_pulled_topology["version"]), pulled_topology
         )
         db_instance.add_key_value_pair_to_db("latest_topology", pulled_topology)
+        logger.debug("Added pulled topo to db")
         # initiate rpc producer with 5 seconds timeout
         rpc_producer = RpcProducer(5, "", "topo")
         response = rpc_producer.call(pulled_topology)
